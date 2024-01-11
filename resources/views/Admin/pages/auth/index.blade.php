5bae3a44c742f0116960fb9527f7c9c8c74f9fb1
Quang Dung <tranquangdung14062001@gmail.com>
2024-01-12 00:39:39 +0700

    121

--- resources/views/Admin/pages/auth/index.blade.php
+++ resources/views/Admin/pages/auth/index.blade.php
@@ -1,6 +1,6 @@
 @extends('Admin.layouts.master')
 
-@section('title', 'Danh mục sản phẩm')
+@section('title', 'Quản lý tài khoản')
 
 @section('content')
 
@@ -10,7 +10,8 @@
             <div class="card border">
                 <div class="card-body">
                     {{-- <h5 class="card-title fw-semibold mb-4">Danh mục sản phẩm</h5> --}}
-                    <h5 class="mb-0 card-title fw-semibold ">Danh mục sản phẩm </h5>
+                    <h5 class="mb-0 card-title fw-semibold ">Quản lý tài khoản</h5>
+
                 </div>
             </div>
         </div>
@@ -21,21 +22,22 @@
                     <div class="row">
 
                         <div class="col-8">
-                            <label>Nhập tên danh mục để tìm kiếm</label>
-                            {{-- <form action="{{ route('categoryIndex') }}" method="get" enctype="multipart/form-data">
+                            <label>Nhập tên tài khoản</label>
+                            <form action="{{ route('Account') }}" method="get" enctype="multipart/form-data">
                                 <div class="input-group">
 
                                     <input class="form-control" type="text" name="search" value=""
-                                        placeholder="nhập tên danh mục">
+                                        placeholder="nhập tên tài khoản">
                                     <button class="btn btn-primary" type="submit"><i class="ti ti-search"></i></button>
                                 </div>
-                            </form> --}}
+                            </form>
                         </div>
-                        <div class="col-4">
-                            {{-- <a href="{{ route('categoryCreate') }}"> <button type="button"
+                        {{-- <div class="col-4">
+
+                            <a href="{{ route('categoryCreate') }}"> <button type="button"
                                     class="btn btn-primary m-1 float-end" title="Thêm mới danh mục"><i
-                                        class="ti ti-plus"></i></button></a> --}}
-                        </div>
+                                        class="ti ti-plus"></i></button></a>
+                        </div> --}}
                     </div>
                     {{-- <p class="mb-0">This is a sample page </p> --}}
                 </div>
@@ -68,67 +70,126 @@
                                         <h6 class="fw-semibold mb-0">STT</h6>
                                     </th>
                                     <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">Tên danh mục</h6>
+                                        <h6 class="fw-semibold mb-0">Tên tài khoản</h6>
                                     </th>
                                     <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">Hành động</h6>
+                                        <h6 class="fw-semibold mb-0">Tên đăng nhập(email)</h6>
+                                    </th>
+                                    <th class="border-bottom-0">
+                                        <h6 class="fw-semibold mb-0">Quyền</h6>
+                                    </th>
+                                    <th class="border-bottom-0">
+                                        <h6 class="fw-semibold mb-0">Khóa tài khoản</h6>
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @foreach ($category as $key => $value)
+                                @foreach ($user as $key => $value)
                                     <tr>
                                         <td class="border-bottom-0">
                                             {{ $key + 1 }}
                                         </td>
                                         <td class="border-bottom-0">
-                                            <span class="fw-normal">{{ $value->name }}</span>
+                                            <span class="fw-normal">
+                                                @if ($value->lock == 0)
+                                                    {{ $value->name }}
+                                                @else
+                                                    <span style="color: red">{{ $value->name }} (đang khóa)</span>
+                                                @endif
+                                            </span>
                                         </td>
                                         <td class="border-bottom-0">
-                                            <a href="{{ route('categoryEdit', $value->id) }}" title="Sửa danh mục"><i
-                                                    class="ti ti-edit fs-8"></i></a>
-                                            <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal_{{ $value->id }}"
-                                                title="Xóa danh mục"><i class="ti ti-trash-x fs-8"></i></a>
-                                                <!-- Modal -->
+                                            <span class="fw-normal">{{ $value->email }}</span>
+                                        </td>
+                                        <td class="border-bottom-0">
+                                            @if ($value->role == 1)
+                                                <span class="fw-normal">Admin</span>
+                                            @elseif ($value->role == 2)
+                                                <span class="fw-normal">User</span>
+                                            @endif
+                                        </td>
+                                        <td class="border-bottom-0">
+                                            @if ($value->lock == 0)
+                                                <a href="" data-bs-toggle="modal"
+                                                    data-bs-target="#exampleModal_{{ $value->id }}"
+                                                    title="Khóa tài khoản">
+                                                    <i class="ti ti-key fs-8"></i>
+                                                </a>
+                                            @else
+                                                <a href="" data-bs-toggle="modal" style="color: red"
+                                                    data-bs-target="#exampleModal_{{ $value->id }}" title="mở tài khoản">
+                                                    <i class="ti ti-key fs-8"></i>
+                                                </a>
+                                            @endif
+                                            <!-- Modal -->
                                             <div class="modal fade" id="exampleModal_{{ $value->id }}" tabindex="-1"
                                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog modal-dialog-centered">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Xóa</h5>
+                                                            @if ($value->lock == 0)
+                                                                <h5 class="modal-title" id="exampleModalLabel">Khóa tài
+                                                                    khoản </h5>
+                                                            @else
+                                                                <h5 class="modal-title" id="exampleModalLabel">Mở khóa tài
+                                                                    khoản </h5>
+                                                            @endif
+
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                                 aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            Bạn có chắc muốn xóa danh mục này không ?
+                                                            @if ($value->lock == 0)
+                                                                Bạn có chắc muốn khóa tài khoản này?
+                                                            @else
+                                                                Bạn có chắc chắn mở khóa tài khoản này chứ?
+                                                            @endif
                                                         </div>
-                                                        <div class="modal-footer">
+                                                        {{-- <div class="modal-footer">
                                                             <button type="button" class="btn btn-secondary"
                                                                 data-bs-dismiss="modal">Hủy</button>
-                                                            <form action="{{ route('categoryDelete', $value->id) }}"
-                                                                method="POST" enctype="multipart/form-data">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button type="submit" class="btn btn-primary">Xóa</button>
-                                                            </form>
-                                                        </div>
+                                                            @if ($value->lock == 0)
+                                                                <form action="{{ route('lock_account') }}" method="POST"
+                                                                    enctype="multipart/form-data">
+                                                                    @csrf
+                                                                    <input type="hidden" class="id" name="id"
+                                                                        id="" value="{{ $value->id }}">
+
+                                                                    <input type="hidden" name="lock" id=""
+                                                                        value="on" {{ $value->lock == 'on' }}>
+                                                                    <button type="submit"
+                                                                        class="btn btn-primary">Khóa</button>
+                                                                </form>
+                                                            @else
+                                                                <form action="{{ route('lock_account') }}" method="POST"
+                                                                    enctype="multipart/form-data">
+                                                                    @csrf
+                                                                    <input type="hidden" class="id" name="id"
+                                                                        id="" value="{{ $value->id }}">
+
+                                                                    <input type="hidden" name="lock" id=""
+                                                                        value="off" {{ $value->lock == 'off' }}>
+
+                                                                    <button type="submit" class="btn btn-primary">Mở
+                                                                        khóa</button>
+                                                                </form>
+                                                            @endif
+                                                        </div> --}}
                                                     </div>
                                                 </div>
                                             </div>
                                         </td>
+
                                     </tr>
-                                @endforeach --}}
+                                @endforeach
+
+
                             </tbody>
                         </table>
                     </div>
-                    {{-- {{ $category->links() }} --}}
+                    {{ $user->links() }}
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
-
 @endsection
