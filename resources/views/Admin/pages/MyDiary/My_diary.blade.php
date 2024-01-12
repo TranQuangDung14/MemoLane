bd3d9476dddc2ff966cc2ed384399550fcd43fa0
Quang Dung <tranquangdung14062001@gmail.com>
2024-01-13 01:34:30 +0700

    quangdung131

--- resources/views/Admin/pages/MyDiary/My_diary.blade.php
+++ resources/views/Admin/pages/MyDiary/My_diary.blade.php
@@ -186,44 +186,15 @@
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
-                            <div class="modal-body" style="max-height: 60vh; overflow-y: auto;">
-                                {{-- @if (isset($value->Comments) && count($value->Comments) > 0)
-                                    @foreach ($value->Comments as $cmt)
-                                        <div class="row">
-                                            <div class="col-md-1 d-flex align-items-center">
-                                                <img src="{{ asset('Admin/') }}/images/profile/user-1.jpg" alt=""
-                                                    width="40" height="40" class="rounded-circle">
-
-                                            </div>
-                                            <div class="col-md-11">
-                                                <div class="mt-3 ms-2">
-                                                    <p>
-                                                        @if ($cmt->User->id === $value->User->id)
-                                                            <a href="{{ route('my_diaryIndex', $value->User->id) }}"><span
-                                                                    style="color:red ">{{ $cmt->User->name }} &ensp;<span
-                                                                        style="color: #009900">tác giả</span> </span></a>
-                                                        @else
-                                                            <a href="{{ route('my_diaryIndex', $cmt->User->id) }}"><span
-                                                                    style="color:blue ">{{ $cmt->User->name }} </span></a>
-                                                        @endif
-
-                                                        &ensp;{{ \Carbon\Carbon::parse($cmt->created_at)->timezone('Asia/Ho_Chi_Minh')->format('H:i') }}&ensp;&nbsp;{{ \Carbon\Carbon::parse($cmt->created_at)->timezone('Asia/Ho_Chi_Minh')->format('d/m/Y') }}
-                                                    </p>
-                                                    <div class="alert alert-secondary">
-                                                        <p>{{ $cmt->content }}</p>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                @else
-                                    <p>Chưa có bình luận nào</p>
-                                @endif --}}
+                            {{-- <div class="modal-body" style="max-height: 60vh; overflow-y: auto;">
+                              
+                            </div> --}}
+                            <div class="modal-body commentsContainer" style="max-height: 60vh; overflow-y: auto;">
                             </div>
+
                             <div class="modal-footer">
                                 {{-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button> --}}
-                                <div class="modal-body">
+                                <div class="container">
                                     <hr>
                                     <div class="row">
                                         <div class="col-md-12 d-flex align-items-center">
@@ -338,6 +309,7 @@
     </div>
     <script>
         $(document).ready(function() {
+            loadComments();
             $('.like-btn').on('click', function(e) {
                 e.preventDefault();
                 var postId = $(this).data('post-id');
@@ -374,29 +346,54 @@
                 });
             });
 
-        });
-
-
-        $(document).ready(function() {
-            $('.openComment').click(function() {
-                // var diary_id = $diary_id;
-                var diary_id = $(this).data('diary-id');
-                console.log('ádad', diary_id);
-                $.ajax({
-                    url: '{{ route('commentLoad') }}',
-                    type: 'GET',
-                    data: {diary_id: diary_id},
-                    success: function(data) {
-                        console.log('data nè', data);
-                        // $('#result').html(data);
-                    },
-                    error: function(error) {
-                        console.log(error);
-                    }
+            function loadComments(diary_id) {
+                // Đặt sự kiện click cho nút mở comment
+                $('.openComment').click(function() {
+                    var clickedDiaryId = $(this).data('diary-id');
+                    console.log('ádad', clickedDiaryId);
+                    $.ajax({
+                        url: '{{ route('commentLoad') }}',
+                        type: 'GET',
+                        data: {
+                            diary_id: clickedDiaryId
+                        },
+                        success: function(data) {
+                            $('#exampleModal_' + clickedDiaryId + ' .modal-body').html(data
+                                .html);
+                            console.log('data nè', data.html);
+                        },
+                        error: function(error) {
+                            console.log(error);
+                        }
+                    });
                 });
-            });
+            }
 
         });
+
+
+        // $(document).ready(function() {
+        //     $('.openComment').click(function() {
+        //         // var diary_id = $diary_id;
+        //         var diary_id = $(this).data('diary-id');
+        //         console.log('ádad', diary_id);
+        //         $.ajax({
+        //             url: '{{ route('commentLoad') }}',
+        //             type: 'GET',
+        //             data: {diary_id: diary_id},
+        //             success: function(data) {
+        //                 // console.log($('#exampleModal_' + diary_id + ' .modal-body').html(data.html));
+        //                 $('#exampleModal_' + diary_id + ' .modal-body').html(data.html);
+        //                 console.log('data nè',data.html);
+        //                 // $('#result').html(data);
+        //             },
+        //             error: function(error) {
+        //                 console.log(error);
+        //             }
+        //         });
+        //     });
+
+        // });
 
         // focus
         //    function focusInput($id) {
